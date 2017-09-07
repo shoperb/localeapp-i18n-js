318fa8f1897f926a60e997f747f61c749d174c71
Myroslava Stavnycha <myroslava@perfectline.co>
2017-09-07 08:32:28 +0300

    routes fix (prev format is not supported with upper rails)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope :module => LocaleappI18nJs do
-    post "localeapp_i18n_js", :to => "LocaleappI18nJs#update"
-    get "localeapp_i18n_js", :to => "LocaleappI18nJs#show"
+    post "localeapp_i18n_js", :to => "localeapp_i18n_js#update"
+    get "localeapp_i18n_js", :to => "localeapp_i18n_js#show"
   end
 end
 
